3fbc36a6a9c5002843bf84a75643c54cd47b01ad
RoboDoig <erskine94@gmail.com>
2025-12-05 19:52:34 +0000

    Add screen definition to schema
    This is more general than this rig and should be moved into a parent ucl-open repository

--- src/ucl_open_hf_visual/rig.py
+++ src/ucl_open_hf_visual/rig.py
@@ -1,12 +1,15 @@
 # Import core types
 from typing import Literal
+from pydantic import Field
 
 from swc.aeon.io import reader
-from swc.aeon.schema import BaseSchema, data_reader
+from swc.aeon.schema import BaseSchema, Device, data_reader
+from ucl_open_hf_visual.visual_stimulation import Screen
 
 from ucl_open_hf_visual import __semver__
 
 
 class UclOpenHfVisualRig(BaseSchema):
     version: Literal[__semver__] = __semver__
+    screen: Screen
     ...
