2e5087c7c2f87b19479233a3c03904557df83d33
RoboDoig <erskine94@gmail.com>
2026-03-19 13:25:25 +0000

    Example with loading all base schemas

--- src/Extensions/LoadSchemas.cs
+++ src/Extensions/LoadSchemas.cs
@@ -5,17 +5,33 @@ using System.Reactive.Linq;
 using Bonsai;
 using UclOpenHfVisualDataSchema;
 
-public class LoadSchemas : Source<UclOpenHfVisualTaskParameters>
+public class LoadSchemas : Source<Tuple<UclOpenSession, UclOpenHfVisualRig, UclOpenHfVisualTaskLogic>>
 {
-    [Description("The relative or absolute path of the file to open for reading.")]
+    [Description("The relative or absolute path of the session file to open for reading.")]
     [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
-    public string Path {get; set;}
+    public string SessionSettingsPath {get; set;}
 
-    public override IObservable<UclOpenHfVisualTaskParameters> Generate()
+    [Description("The relative or absolute path of the rig file to open for reading.")]
+    [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+    public string RigSettingsPath {get; set;}
+
+    [Description("The relative or absolute path of the task file to open for reading.")]
+    [Editor("Bonsai.Design.OpenFileNameEditor, Bonsai.Design", DesignTypes.UITypeEditor)]
+    public string TaskLogicSettingsPath {get; set;}
+
+    public override IObservable<Tuple<UclOpenSession, UclOpenHfVisualRig, UclOpenHfVisualTaskLogic>> Generate()
     {
-        string file = File.ReadAllText(Path);
-        UclOpenHfVisualTaskParameters taskParameters = Newtonsoft.Json.JsonConvert.DeserializeObject<UclOpenHfVisualTaskParameters>(file);
+        string sessionSettingsRaw = File.ReadAllText(SessionSettingsPath);
+        string rigSettingsRaw = File.ReadAllText(RigSettingsPath);
+        string taskLogicSettingsRaw = File.ReadAllText(TaskLogicSettingsPath);
 
-        return Observable.Return(taskParameters);
+        var sessionSource = Observable.Return(Newtonsoft.Json.JsonConvert.DeserializeObject<UclOpenSession>(sessionSettingsRaw));
+        var rigSource = Observable.Return(Newtonsoft.Json.JsonConvert.DeserializeObject<UclOpenHfVisualRig>(rigSettingsRaw));
+        var taskLogicSource = Observable.Return(Newtonsoft.Json.JsonConvert.DeserializeObject<UclOpenHfVisualTaskLogic>(taskLogicSettingsRaw));
+
+        return sessionSource.Zip(rigSource, taskLogicSource, (s1, s2, s3) =>
+        {
+            return Tuple.Create(s1, s2, s3);
+        });
     }
 }
